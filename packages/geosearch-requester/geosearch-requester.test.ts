f4b57edbeee82d969e10f9e51b959940f50ff5b3
Atul Varma <varmaa@gmail.com>
2019-10-17 09:55:29 -0400

    Add test suites and common configuration (#3)
    This gives us common testing infrastructure across projects using Jest, along with simple test suites for our packages.

---
+++ packages/geosearch-requester/geosearch-requester.test.ts
@@ -0,0 +1,17 @@
+import { GeoSearchRequester, GeoSearchRequesterOptions } from "./geosearch-requester";
+
+function makeOpts(opts?: Partial<GeoSearchRequesterOptions>): GeoSearchRequesterOptions {
+  return {
+    fetch: jest.fn(),
+    onError: jest.fn(),
+    onResults: jest.fn(),
+    ...opts
+  };
+}
+
+describe("GeoSearchRequester", () => {
+  it("throws an error when fetch implementation doesn't exist", () => {
+    expect(() => new GeoSearchRequester(makeOpts({fetch: undefined})))
+      .toThrow(/A fetch implementation was not passed to GeoSearchRequester, and one does not exist in the global scope/i);
+  });
+});
