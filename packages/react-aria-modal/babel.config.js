56c7698607b9f812c13c06d3f6075d44d9ced043
Atul Varma <varmaa@gmail.com>
2020-03-29 06:08:14 -0400

    Port over focus-trap-react tests.

--- packages/react-aria-modal/babel.config.js
+++ packages/react-aria-modal/babel.config.js
@@ -1,1 +1,9 @@
-module.exports = require('../../babel.config');
+const base = require('../../babel.config');
+
+module.exports = {
+  ...base,
+  presets: [
+    ...base.presets,
+    "@babel/preset-react",
+  ],
+}
