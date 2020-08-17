83cc90a4c38002cfce4eaa08d6fcb86262989283
Sam R <sam@MacBook-Pro.local>
2020-08-17 17:17:29 -0400

    added comment

--- packages/react-common/src/moratorium-banner.tsx
+++ packages/react-common/src/moratorium-banner.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+/** 
+ * Renders a banner of text announcing JustFix's status during COVID-19 and details of the NYC Eviction Moratorium. 
+ * The optional "locale" property is used for localization. If the property is set to an accepted locale (like "es"), 
+ * the resulting blurb of text will be rendered in that given language. If the property is undefined or not an accepted 
+ * locale, the English version of the blurb is returned.
+ * 
+ * Note: the component is completely unstyled, i.e. styling it is the responsibility of the caller.
+*/
 export const CovidMoratoriumBanner: React.FC<{ locale?: string }> = ({
   locale,
 }) => {
