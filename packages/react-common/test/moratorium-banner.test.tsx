37c22bee343f620d5444bf88c6b821179f86464d
Sam R <sam@MacBook-Pro.local>
2020-08-17 16:19:38 -0400

    made testing suite more DRY

--- packages/react-common/test/moratorium-banner.test.tsx
+++ packages/react-common/test/moratorium-banner.test.tsx
@@ -2,38 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { CovidMoratoriumBanner } from "../src";
 
-test("Moratorium Banner renders English text by default", () => {
-  const container = document.createElement("div");
-  document.body.appendChild(container);
-  try {
+describe("Moratorium Banner ", () => {
+  let container: any;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    ReactDOM.unmountComponentAtNode(container);
+    document.body.removeChild(container);
+  });
+
+  test("renders English text by default", () => {
     ReactDOM.render(<CovidMoratoriumBanner />, container);
     expect(container.innerHTML).toContain("COVID-19 Update:");
-  } finally {
-    ReactDOM.unmountComponentAtNode(container);
-    document.body.removeChild(container);
-  }
-});
+  });
 
-test("Moratorium Banner renders Spanish for 'es' locale specified", () => {
-  const container = document.createElement("div");
-  document.body.appendChild(container);
-  try {
+  test("renders Spanish for 'es' locale specified", () => {
     ReactDOM.render(<CovidMoratoriumBanner locale="es" />, container);
     expect(container.innerHTML).toContain("Actualización COVID-19:");
-  } finally {
-    ReactDOM.unmountComponentAtNode(container);
-    document.body.removeChild(container);
-  }
-});
+  });
 
-test("Moratorium Banner falls back on English for unknown locales", () => {
-  const container = document.createElement("div");
-  document.body.appendChild(container);
-  try {
+  test("falls back on English for unknown locales", () => {
     ReactDOM.render(<CovidMoratoriumBanner locale="tr" />, container);
     expect(container.innerHTML).toContain("COVID-19 Update:");
-  } finally {
-    ReactDOM.unmountComponentAtNode(container);
-    document.body.removeChild(container);
-  }
+  });
 });
