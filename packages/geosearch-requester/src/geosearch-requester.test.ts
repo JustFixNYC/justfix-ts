1dd048696f717876c768b4165e2caf9058576dff
Sam Rabiyah <sraby@users.noreply.github.com>
2020-09-03 15:42:15 -0400

    Add custom GeoSearch Autocomplete url option (#13)
    This PR adds a new optional constructor parameter called customGeoAutocompleteUrl to our GeoSearchRequester class. This allows the user to override the standard NYC Planning Labs Autocomplete url, which will let us try out our own hosted GeoSearch tool!

--- packages/geosearch-requester/src/geosearch-requester.test.ts
+++ packages/geosearch-requester/src/geosearch-requester.test.ts
@@ -14,4 +14,12 @@ describe("GeoSearchRequester", () => {
     expect(() => new GeoSearchRequester(makeOpts({fetch: undefined})))
       .toThrow(/A fetch implementation was not passed to GeoSearchRequester, and one does not exist in the global scope/i);
   });
+  it("allows for custom GeoAutocomplete urls to be passed in", () => {
+    var r = new GeoSearchRequester(makeOpts({customGeoAutocompleteUrl: "https://www.boop.com/autocomplete"}));
+    expect(r.searchQueryToURL("Boopy Boop")).toBe("https://www.boop.com/autocomplete?text=Boopy%20Boop");
+  });
+  it("uses the standard GeoAutocomplete url when no custom one is defined", () => {
+    var r = new GeoSearchRequester(makeOpts());
+    expect(r.searchQueryToURL("Boopy Boop")).toBe("https://geosearch.planninglabs.nyc/v1/autocomplete?text=Boopy%20Boop");
+  });
 });
