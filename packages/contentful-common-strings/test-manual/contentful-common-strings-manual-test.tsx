fe1a6ad262bf518f51d9e33f42134e27ce21c988
Atul Varma <varmaa@gmail.com>
2021-06-24 07:38:13 -0400

    Add configuration form to manual test page.

--- packages/contentful-common-strings/test-manual/contentful-common-strings-manual-test.tsx
+++ packages/contentful-common-strings/test-manual/contentful-common-strings-manual-test.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { getHTMLElement } from "@justfixnyc/util";
 import {
+  ContentfulCommonStringsConfig,
   ContentfulCommonStringsEntry,
+  DEFAULT_CONTENTFUL_COMMON_STRINGS_CONFIG,
   fetchContentfulCommonStrings,
 } from "../src";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+
+const CONFIG_KEYS: (keyof ContentfulCommonStringsConfig)[] = Object.keys(
+  DEFAULT_CONTENTFUL_COMMON_STRINGS_CONFIG
+) as any[];
 
 const container = getHTMLElement("div", "#root");
 
@@ -21,9 +27,9 @@ const CommonStringsForId: React.FC<{
 
       return (
         <div key={locale}>
-          <h2>
-            Common string <code>{id}</code> ({locale})
-          </h2>
+          <h3>
+            <code>{id}</code> ({locale})
+          </h3>
           <div style={{ paddingLeft: "2em" }}>
             {documentToReactComponents(doc)}
           </div>
@@ -33,11 +39,57 @@ const CommonStringsForId: React.FC<{
   </>
 );
 
+const ConfigForm: React.FC<{ config: ContentfulCommonStringsConfig }> = ({
+  config,
+}) => (
+  <form method="get">
+    <dl>
+      {CONFIG_KEYS.map((key) => (
+        <React.Fragment key={key}>
+          <dt>
+            <label htmlFor={key}>
+              <code>{key}</code>
+            </label>
+          </dt>
+          <dd>
+            <input
+              name={key}
+              id={key}
+              type="text"
+              style={{ fontFamily: "monospace", width: "25em" }}
+              defaultValue={config[key]}
+            />
+          </dd>
+        </React.Fragment>
+      ))}
+    </dl>
+    <button type="submit">Submit</button>
+  </form>
+);
+
+function getConfigFromQuerystring() {
+  const config = DEFAULT_CONTENTFUL_COMMON_STRINGS_CONFIG;
+  const search = new URLSearchParams(window.location.search);
+
+  for (let key of CONFIG_KEYS) {
+    const value = search.get(key);
+    if (value) {
+      config[key] = value;
+    }
+  }
+
+  return config;
+}
+
 async function main() {
+  const config = getConfigFromQuerystring();
   const map = await fetchContentfulCommonStrings();
 
-  const el = ReactDOM.render(
+  ReactDOM.render(
     <>
+      <h2>Configuration</h2>
+      <ConfigForm config={config} />
+      <h2>Common strings</h2>
       <p>
         Below are all the Contentful common strings for all available locales.
       </p>
